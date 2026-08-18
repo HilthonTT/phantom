57507aaae2b5b842f394947957c363fb0148d427
HilthonTT <timothy.tandt@outlook.be>
2026-08-18 22:29:37 +0200

    fixed formatting

--- crates/phantom-core/src/utils/math.rs
+++ crates/phantom-core/src/utils/math.rs
@@ -42,4 +42,6 @@ macro_rules! impl_tried {
     )+};
 }
 
-impl_tried!(u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize);
+impl_tried!(
+    u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize
+);
